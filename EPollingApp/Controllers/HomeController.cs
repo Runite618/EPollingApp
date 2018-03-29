d8ccbaddcb55cbc43c2f26cb3c0aa4a018d1fb44
Matthew Mo <matthew_mo@hotmail.co.uk>
2018-03-29 15:17:02 +0100

    More appropriate layout for E-polling app

---
+++ EPollingApp/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EPollingApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            ViewBag.Message = "Register new polling user here";
+
+            return View();
+        }
+    }
+}
